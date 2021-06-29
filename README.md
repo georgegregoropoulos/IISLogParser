d715e8a230cee7c5fcf1bd3f79b8a9ee94400883
GeorgeG <george_gregoropoulos@hotmail.com>
2021-06-29 20:28:41 +0300

    custom execution status strip

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # IISLogParser
-Parses IIS logs files and produces some standard views, like:
+Parses standard IIS log files and produces some standard views, like:
   Average execution time,
   Execution time Avg Min Max,
-  Execution time more than 10s and many more
+  Execution time more than 10s,
+  .. and many more.
   
 Shows the following statistics per URL:
   Number of hits over time,
--- frmCustomSql.cs
+++ frmCustomSql.cs
@@ -8,49 +8,47 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace IISLogParser
-{
-  public partial class frmCustomSql : Form
-  {
-    private List<string> _LogFiles = null;
-    public frmCustomSql()
-    {
-      InitializeComponent();
+namespace IISLogParser {
+    public partial class frmCustomSql : Form {
+        private List<string> _LogFiles = null;
+        public frmCustomSql() {
+            InitializeComponent();
+            toolStripStatusLabel.Text = string.Empty;
+        }
+        public frmCustomSql(List<string> sLogFiles) {
+            InitializeComponent();
 
+            _LogFiles = sLogFiles;
+            
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e) {
+            if (String.IsNullOrEmpty(rtExecutionScript.Text)) return;
+
+            try {
+                statusStrip.Text = "Executing..";
+                LogParser.LogParser lp = new LogParser.LogParser(_LogFiles);
+
+                List<string> scripts = new List<string>();
+                scripts.Add(rtExecutionScript.Text);
+
+                Dictionary<string, string> res = lp.Run(scripts);
+
+                if (res.ContainsKey("customScript_0")) {
+                    rtResults.Text = res["customScript_0"];
+                    toolStripStatusLabel.Text = "Finished.";
+                }
+                else {
+                    toolStripStatusLabel.Text = "No results.";
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e) {
+            Close();
+        }
     }
-
-
-    public frmCustomSql(List<string> sLogFiles)
-    {
-      InitializeComponent();
-
-      _LogFiles = sLogFiles;
-    }
-
-    private void btnExecute_Click(object sender, EventArgs e)
-    {
-      if (String.IsNullOrEmpty(rtExecutionScript.Text)) return;
-
-      try
-      {
-        LogParser.LogParser lp = new LogParser.LogParser(_LogFiles);
-
-        List<string> scripts = new List<string>();
-        scripts.Add(rtExecutionScript.Text);
-
-        Dictionary<string, string> res = lp.Run(scripts);
-
-        rtResults.Text = res["customScript_0"];
-      }
-      catch (Exception ex)
-      {
-        MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-      }
-    }
-
-    private void btnClose_Click(object sender, EventArgs e)
-    {
-      Close();
-    }
-  }
 }
